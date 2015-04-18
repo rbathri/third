30f85b0ed4931c836419ec18a3128315d626be99
Bathrinath Raveendran <bathrinath_raveendran@intuit.com>
2015-04-18 15:37:05 +0530

    with javadoc comments

--- src/main/java/com/springpeople/App.java
+++ src/main/java/com/springpeople/App.java
@@ -1,11 +1,12 @@
 package com.springpeople;
 
 /**
- * Hello world!
+ * Hello world Class file!
  *
  */
 public class App 
 {
+/** Hellow world Main method */
     public static void main( String[] args )
     {
         System.out.println( "Hello World! with GitHUB" );
